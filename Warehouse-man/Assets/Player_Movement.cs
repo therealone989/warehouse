2e32fec5e1bb6c0421a761e6db23673b4cc71a27
therealone989 <mrfabigm@gmail.com>
2026-08-30 12:35:05 +0200

    Player Look angepasst

--- Warehouse-man/Assets/Player_Movement.cs
+++ Warehouse-man/Assets/Player_Movement.cs
@@ -1,32 +1,84 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
 
-    public float moveSpeed = 100f;
-    Rigidbody rb;
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Mouse")]
+    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private Transform head;
+
+    private Rigidbody rb;
 
     private float x;
     private float z;
-    private Vector3 movement;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float yaw;
+    private float pitch;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        yaw = transform.eulerAngles.y;
     }
 
     void Update()
     {
+        // WASD
         x = Input.GetAxisRaw("Horizontal");
         z = Input.GetAxisRaw("Vertical");
+
+
+        // Maus
+        mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+
+        // Links / Rechts
+        yaw += mouseX;
+
+
+        // Hoch / Runter
+        pitch -= mouseY;
+
+        // Verhindert komplettes Überschlagen
+        pitch = Mathf.Clamp(pitch, -85f, 85f);
+
+
+        // Nur Head hoch/runter drehen
+        head.localRotation = Quaternion.Euler(pitch, 0, 0);
     }
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(x, 0, z).normalized;
+        // Zielrotation des Players
+        Quaternion rotation = Quaternion.Euler(0, yaw, 0);
 
-        rb.linearVelocity = new Vector3(movement.x * moveSpeed,0,movement.z * moveSpeed) * Time.deltaTime;
+        rb.MoveRotation(rotation);
 
-        Debug.Log(rb.linearVelocity);
+
+        // Lokale WASD Richtung
+        Vector3 inputDirection = new Vector3(x, 0, z).normalized;
+
+        // Richtung entsprechend Playerrotation drehen
+        Vector3 movement = rotation * inputDirection;
+
+
+        // Geschwindigkeit setzen
+        rb.linearVelocity = new Vector3(
+            movement.x * moveSpeed,
+            rb.linearVelocity.y,
+            movement.z * moveSpeed
+        );
     }
 }
